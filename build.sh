0129d34b71abe89d0319260c86a35be1391877b5
Sanjaya Ratnaweera <pini@apache.org>
2007-03-26 12:19:22 +0000

    Changed the build system for new repository

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ./autogen.sh
-./configure --prefix=${AXIS2C_HOME} --enable-static=no 
+./configure --prefix=${AXIS2C_HOME}/modules --enable-static=no --with-axis2=${AXIS2C_HOME}/include
 make
-make install
+#make install
