8c7167473ced182b485f2f038b9eb8795d90dac0
Pallewela Perera <dumindu@apache.org>
2007-04-25 09:28:39 +0000

    Fixing win32 build

--- build/win32/deploy_rampart.bat
+++ build/win32/deploy_rampart.bat
@@ -7,6 +7,12 @@ copy bin\samples\services\sec_echo\sec_echo.dll %AXIS2C_HOME%\services\sec_echo\
 copy bin\samples\services\sec_echo\services.xml %AXIS2C_HOME%\services\sec_echo\
 
 md %AXIS2C_HOME%\bin\samples\rampart\
-md %AXIS2C_HOME%\bin\samples\rampart\authn_provider
+md %AXIS2C_HOME%\bin\samples\rampart\authn_provider\
 copy bin\samples\authn_provider\authn.dll %AXIS2C_HOME%\bin\samples\rampart\authn_provider\
 
+md %AXIS2C_HOME%\bin\samples\rampart\client\
+md %AXIS2C_HOME%\bin\samples\rampart\client\sec_echo\
+copy bin\samples\client\sec_echo\echo.exe %AXIS2C_HOME%\bin\samples\rampart\client\sec_echo
+
+md %AXIS2C_HOME%\bin\samples\rampart\callback
+copy bin\samples\callback\pwcb.dll %AXIS2C_HOME%\bin\samples\rampart\callback
