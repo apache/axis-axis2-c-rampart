deda8eab9daa437162fda5b4faaec9d1c88af897
Malinda Kaushalye Kapuruge <kaushalye@apache.org>
2007-06-07 09:23:49 +0000

    Copying services.xml

--- samples/secpolicy/win_deploy.bat
+++ samples/secpolicy/win_deploy.bat
@@ -1,9 +1,10 @@
 @echo off
 SET scn=scenario5
-IF [NOT] [%*]==[/?] SET [%scn%]=%*
-
 echo Deploying %scn%
+echo Copying %scn%\client policy file
 copy  %scn%\client-policy.xml %AXIS2C_HOME%\client_repo\policy.xml
+echo Copying %scn%\services.xml
+copy  %scn%\services.xml %AXIS2C_HOME%\services\sec_echo\services.xml
 echo Please change path name settings in following files before running the client
 echo   1. %AXIS2C_HOME%\client_repo\policy.xml
 echo   2. %AXIS2C_HOME%\services\sec_echo\services.xml
