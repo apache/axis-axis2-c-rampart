46acacda1edb1949d605cf517b20a40573b36b9a
Bamunavita Gamage Sahan Sajeewa <sahan@apache.org>
2007-06-10 14:48:18 +0000

    Making the deploy.sh portable

--- samples/secpolicy/deploy.sh
+++ samples/secpolicy/deploy.sh
@@ -11,16 +11,26 @@ SERVICE_HOME="$AXIS2C_HOME/services/sec_echo/"
 #COPYING THE RELEVENT POLICY FILES TO CLIENT AND SERVER
 
 #copy client policy file to CLIENT_REPO
-echo "Copying client policy files to $CLIENT_REPO"
-cp $1/client-policy.xml $CLIENT_REPO/policy.xml
+#echo "Copying client policy files to $CLIENT_REPO"
+#cp $1/client-policy.xml $CLIENT_REPO/policy.xml
 
 echo "Replacing settings in policy files."
-sed -i 's,AXIS2C_HOME,'$AXIS2C_HOME',g' $CLIENT_REPO/policy.xml
+if [ `uname -s` = Darwin ]
+then
+    sed -e 's,AXIS2C_HOME,'$AXIS2C_HOME',g' -e 's,\.so,\.dylib,g' $1/client-policy.xml > $CLIENT_REPO/policy.xml
+else
+    sed 's,AXIS2C_HOME,'$AXIS2C_HOME',g' $1/client-policy.xml > $CLIENT_REPO/policy.xml
+fi
 
 #copy services.xml to SERVICE_HOME
-echo "Copying services.xml to $SERVICE_HOME"
-cp $1/services.xml $SERVICE_HOME/services.xml
+#echo "Copying services.xml to $SERVICE_HOME"
+#cp $1/services.xml $SERVICE_HOME/services.xml
 
 echo "Replacing settings in Configuration files."
-sed -i 's,AXIS2C_HOME,'$AXIS2C_HOME',g' $SERVICE_HOME/services.xml
+if [ `uname -s` = Darwin ]
+then
+    sed -e 's,AXIS2C_HOME,'$AXIS2C_HOME',g' -e 's,\.so,\.dylib,g' $1/services.xml > $SERVICE_HOME/services.xml
+else
+    sed 's,AXIS2C_HOME,'$AXIS2C_HOME',g' $1/services.xml > $SERVICE_HOME/services.xml
+fi
 
