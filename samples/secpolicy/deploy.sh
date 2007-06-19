801196766fa552ec97c5b76f2e4e9c88b4e0b95e
Malinda Kaushalye Kapuruge <kaushalye@apache.org>
2007-06-19 09:07:05 +0000

    Changing deploy script

--- samples/secpolicy/deploy.sh
+++ samples/secpolicy/deploy.sh
@@ -5,32 +5,26 @@ then
     exit
 fi
 
-CLIENT_REPO="$AXIS2C_HOME/client_repo"
-SERVICE_HOME="$AXIS2C_HOME/services/sec_echo"
+INST_DIR=$AXIS2C_HOME
+CLIENT_REPO="$INST_DIR/client_repo"
+SERVICE_HOME="$INST_DIR/services/sec_echo"
 
 #COPYING THE RELEVENT POLICY FILES TO CLIENT AND SERVER
 
-#copy client policy file to CLIENT_REPO
-#echo "Copying client policy files to $CLIENT_REPO"
-#cp $1/client-policy.xml $CLIENT_REPO/policy.xml
 
 echo "Replacing settings in policy files."
 if [ `uname -s` = Darwin ]
 then
-    sed -e 's,AXIS2C_HOME,'$AXIS2C_HOME',g' -e 's,\.so,\.dylib,g' $1/client-policy.xml > $CLIENT_REPO/policy.xml
+    sed -e 's,AXIS2C_HOME,'$INST_DIR',g' -e 's,\.so,\.dylib,g' $1/client-policy.xml > $CLIENT_REPO/policy.xml
 else
-    sed 's,AXIS2C_HOME,'$AXIS2C_HOME',g' $1/client-policy.xml > $CLIENT_REPO/policy.xml
+    sed 's,AXIS2C_HOME,'$INST_DIR',g' $1/client-policy.xml > $CLIENT_REPO/policy.xml
 fi
-
-#copy services.xml to SERVICE_HOME
-#echo "Copying services.xml to $SERVICE_HOME"
-#cp $1/services.xml $SERVICE_HOME/services.xml
 
 echo "Replacing settings in Configuration files."
 if [ `uname -s` = Darwin ]
 then
-    sed -e 's,AXIS2C_HOME,'$AXIS2C_HOME',g' -e 's,\.so,\.dylib,g' $1/services.xml > $SERVICE_HOME/services.xml
+    sed -e 's,AXIS2C_HOME,'$INST_DIR',g' -e 's,\.so,\.dylib,g' $1/services.xml > $SERVICE_HOME/services.xml
 else
-    sed 's,AXIS2C_HOME,'$AXIS2C_HOME',g' $1/services.xml > $SERVICE_HOME/services.xml
+    sed 's,AXIS2C_HOME,'$INST_DIR',g' $1/services.xml > $SERVICE_HOME/services.xml
 fi
 
