f23ee1bd8a4a7142f456b64e73e2534d5cd3fb99
Pallewela Perera <dumindu@apache.org>
2007-09-24 11:16:52 +0000

    changed the binary distribution header location to include/rampart-1.0.0

--- build/win32/make_bin_dist.bat
+++ build/win32/make_bin_dist.bat
@@ -4,12 +4,13 @@ mkdir %BINDIST%
 mkdir %BINDIST%\samples
 mkdir %BINDIST%\modules
 mkdir %BINDIST%\include
+mkdir %BINDIST%\include\rampart-1.0.0
 mkdir %BINDIST%\docs
 
 
 xcopy /E /I /Y .\bin\samples\*.* %BINDIST%\samples\
 xcopy /E /I /Y .\modules\*.* %BINDIST%\modules\
-xcopy /E /I /Y .\include\*.* %BINDIST%\include\
+xcopy /E /I /Y .\include\*.* %BINDIST%\include\rampart-1.0.0\
 xcopy /E /I /Y .\docs\*.* %BINDIST%\docs\
 
 xcopy README %BINDIST%
