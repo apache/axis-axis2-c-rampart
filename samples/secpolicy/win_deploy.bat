ed760ed9d0008cbdaa609ccf9bcd60506579a702
Malinda Kaushalye Kapuruge <kaushalye@apache.org>
2007-09-24 08:34:28 +0000

    removed pause in win_deploy.bat

--- samples/secpolicy/win_deploy.bat
+++ samples/secpolicy/win_deploy.bat
@@ -11,5 +11,4 @@ copy  %scn%\services.xml %AXIS2C_HOME%\services\sec_echo\services.xml
 echo Please change path name settings in following files before running the client
 echo   1. %AXIS2C_HOME%\client_repo\policy.xml
 echo   2. %AXIS2C_HOME%\services\sec_echo\services.xml
-pause
 @echo on
