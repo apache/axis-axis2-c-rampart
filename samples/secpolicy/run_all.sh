07c36c89a011867d1588ba584eb33ab6510d6dfe
Milinda Pathirage <milinda@apache.org>
2008-02-18 09:14:24 +0000

    Fixing a typo.

--- samples/secpolicy/run_all.sh
+++ samples/secpolicy/run_all.sh
@@ -5,7 +5,7 @@ _PORT=9090
 _SLEEP=3
 #You may change these to scenarios u need to run
 _LST="1 2 3 4 5 6 7 8 9 10 11 12 13 15 16 17 18 19"
-_SAML="14"
+_SAML="20"
 if [ $# -eq 1 ]
 then
     _PORT=$1
