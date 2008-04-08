7095efc5eee581057471b8890e4a103ffebabcd7
Selvaratnam Uthaiyashankar <shankar@apache.org>
2008-04-08 08:07:05 +0000

    version number change to 1.2.0

--- build/win32/make_bin_dist.bat
+++ build/win32/make_bin_dist.bat
@@ -1,17 +1,17 @@
-set BINDIST=..\rampartc-bin-1.1.0-win32
+set BINDIST=..\rampartc-bin-1.2.0-win32
 if exist %BINDIST% rd /s /q %BINDIST%
 mkdir %BINDIST%
 mkdir %BINDIST%\bin
 mkdir %BINDIST%\bin\samples
 mkdir %BINDIST%\modules
 mkdir %BINDIST%\include
-mkdir %BINDIST%\include\rampart-1.1.0
+mkdir %BINDIST%\include\rampart-1.2.0
 mkdir %BINDIST%\docs
 
 
 xcopy /E /I /Y .\bin\samples\*.* %BINDIST%\bin\samples\
 xcopy /E /I /Y .\modules\*.* %BINDIST%\modules\
-xcopy /E /I /Y .\include\*.* %BINDIST%\include\rampart-1.1.0\
+xcopy /E /I /Y .\include\*.* %BINDIST%\include\rampart-1.2.0\
 xcopy /E /I /Y .\docs\*.* %BINDIST%\docs\
 
 xcopy README %BINDIST%
