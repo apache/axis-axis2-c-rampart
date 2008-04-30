c7821bfbd94320f356971142c033d7b8c71bc6c0
Malinda Kaushalye Kapuruge <kaushalye@apache.org>
2008-04-30 06:37:54 +0000

    Changes due to the version change in Axis2/C

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 ./autogen.sh
-./configure --prefix=${AXIS2C_HOME} --enable-static=no --with-axis2=${AXIS2C_HOME}/include/axis2-1.3.1
+./configure --prefix=${AXIS2C_HOME} --enable-static=no --with-axis2=${AXIS2C_HOME}/include/axis2-1.4.0
 make
 make install
