5b64aa6fc816ad0d349f31dfaf274855b95c6c29
Supun Kamburugamuva <supun@apache.org>
2008-05-27 07:51:03 +0000

    Adding scenario23 and changes to run_all.js to incorporate the sample

--- samples/secpolicy/run_all.js
+++ samples/secpolicy/run_all.js
@@ -1,6 +1,6 @@
 var WshShell = new ActiveXObject("WScript.Shell");
 var fso = new ActiveXObject("Scripting.FileSystemObject");
-var server_port = 9090;
+var server_port = 7070;
 
 var f, s, re, ss, r;
 var ForReading = 1, ForWriting = 2;
@@ -9,6 +9,27 @@ var axis2c_home = WshShell.ExpandEnvironmentStrings("%AXIS2C_HOME%");
 var client_repo = axis2c_home + "\\client_repo";
 
 for (var i = 1; i <= 14; i++) {
+    run(i);
+}
+
+WScript.Echo("Scenario " + 20 + ":");
+deploy(20);
+deploy_20();
+WScript.Echo("services.xml and policy.xml deployed");
+var http_server = WshShell.Exec(axis2c_home + "\\bin\\axis2_http_server.exe -p " + server_port + " -r " + axis2c_home);
+WScript.Sleep(3000);    		
+WScript.Echo("Started Axis2C simple HTTP server\n\n");
+var client = WshShell.Exec(axis2c_home + "\\bin\\samples\\rampart\\client\\issued_token\\echo.exe http://localhost:9090/axis2/services/sec_echo/echoString " + client_repo);
+s = client.StdOut.ReadAll();
+WScript.Echo(s);
+http_server.Terminate();
+
+for (var i = 21; i <= 23; i++) {
+    run(i);
+}
+
+function run(i)
+{
     WScript.Echo("Scenario " + i + ":");    
     deploy(i);
     WScript.Echo("services.xml and policy.xml deployed");
@@ -25,18 +46,6 @@ for (var i = 1; i <= 14; i++) {
     WScript.Echo(s);
     http_server.Terminate();
 }
-
-WScript.Echo("Scenario " + 20 + ":");
-deploy(20);
-deploy_20();
-WScript.Echo("services.xml and policy.xml deployed");
-var http_server = WshShell.Exec(axis2c_home + "\\bin\\axis2_http_server.exe -p " + server_port + " -r " + axis2c_home);
-WScript.Sleep(3000);    		
-WScript.Echo("Started Axis2C simple HTTP server\n\n");
-var client = WshShell.Exec(axis2c_home + "\\bin\\samples\\rampart\\client\\issued_token\\echo.exe http://localhost:9090/axis2/services/sec_echo/echoString " + client_repo);
-s = client.StdOut.ReadAll();
-WScript.Echo(s);
-http_server.Terminate();
 
 function replace()   
 {
