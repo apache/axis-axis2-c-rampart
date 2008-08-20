de08ff5013970f26df5380afc1ad7f44136569dc
Selvaratnam Uthaiyashankar <shankar@apache.org>
2008-08-20 13:21:31 +0000

    adding test cases for WS-SecPolicy 1.2 and WS-SecConv1.3

--- samples/secpolicy/run_all.bat
+++ samples/secpolicy/run_all.bat
@@ -7,7 +7,7 @@ echo Using port %_PORT% to start the server
 
 
 
-for %%i in (1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 21 22 23 25 26) do call test_scen.bat %_SCEN%%%i %_PORT% off
+for %%i in (1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 21 22 23 25 26 27 28) do call test_scen.bat %_SCEN%%%i %_PORT% off
     
 taskkill /F /IM axis2_http_server.exe
 echo DONE
--- samples/secpolicy/run_all.sh
+++ samples/secpolicy/run_all.sh
@@ -4,7 +4,7 @@ _SMPL_DIR="$PWD"
 _PORT=9090
 _SLEEP=2
 #You may change these to scenarios u need to run
-_LST="1 2 3 4 5 6 7 8 9 10 11 12 13 15 16 17 18 19 21 22 23 25 26"
+_LST="1 2 3 4 5 6 7 8 9 10 11 12 13 15 16 17 18 19 21 22 23 25 26 27 28"
 
 if [ $# -eq 1 ]
 then
