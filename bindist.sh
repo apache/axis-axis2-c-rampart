45578167bd3d65e347c5f6978caf100dd49a4176
Selvaratnam Uthaiyashankar <shankar@apache.org>
2009-05-11 09:57:32 +0000

    modified bindist.sh according to the new folder structure

--- bindist.sh
+++ bindist.sh
@@ -31,53 +31,49 @@ sleep 1
 
 echo "Creating directories in $PWDIR"
 mkdir $BIN_DIR
-mkdir $BIN_DIR/bin
-mkdir $BIN_DIR/bin/samples
-mkdir $BIN_DIR/bin/samples/server
-mkdir $BIN_DIR/bin/samples/secpolicy
-mkdir $BIN_DIR/modules
-mkdir $BIN_DIR/modules/rampart
 mkdir $BIN_DIR/samples
+mkdir $BIN_DIR/samples/bin
+mkdir $BIN_DIR/samples/bin/rampartc
+mkdir $BIN_DIR/samples/lib
+mkdir $BIN_DIR/samples/lib/rampartc
+mkdir $BIN_DIR/samples/src
+mkdir $BIN_DIR/samples/src/rampartc
 mkdir $BIN_DIR/include
 mkdir $BIN_DIR/include/$INCL_V_DIR
+mkdir $BIN_DIR/modules
+mkdir $BIN_DIR/modules/rampart
+mkdir $BIN_DIR/modules/rahas
 mkdir $BIN_DIR/lib
+mkdir $BIN_DIR/services
 
 echo "Copy related files to $BIN_DIR"
 #Copy other related files
 cp AUTHORS $BIN_DIR
+cp ChangeLog $BIN_DIR
 cp COPYING $BIN_DIR
 cp INSTALL $BIN_DIR
 cp LICENSE $BIN_DIR
 cp NEWS $BIN_DIR
-cp README $BIN_DIR
 cp NOTICE $BIN_DIR
+cp README $BIN_DIR
 
 echo "Copy rampart module"
 #Copy rampart module
 cp -r $AXIS2C_HOME/modules/rampart $BIN_DIR/modules/
+cp -r $AXIS2C_HOME/modules/rahas $BIN_DIR/modules/
 
 echo "Copy libraries"
-cp -d $AXIS2C_HOME/lib/liboxstokens.* $BIN_DIR/lib
-cp -d $AXIS2C_HOME/lib/libomxmlsec.* $BIN_DIR/lib
-cp -d $AXIS2C_HOME/lib/libomopenssl.* $BIN_DIR/lib
-cp -d $AXIS2C_HOME/lib/libsaml.* $BIN_DIR/lib
-cp -d $AXIS2C_HOME/lib/libsecconv.* $BIN_DIR/lib
-cp -d $AXIS2C_HOME/lib/libtrust.* $BIN_DIR/lib
-cp -d $AXIS2C_HOME/lib/libmod_rampart.* $BIN_DIR/lib
+cp -d $AXIS2C_HOME/lib/librampart.so.* $BIN_DIR/lib
 
 echo "Strip binaries"
 strip $BIN_DIR/lib/*.so
 
 echo "Copy samples"
-#copy samples
-#cp -r samples/secpolicy/* $BIN_DIR/bin/samples/secpolicy/
-cp -r $AXIS2C_HOME/bin/samples/rampart/secpolicy/* $BIN_DIR/bin/samples/secpolicy/
-cp -r $AXIS2C_HOME/bin/samples/rampart/* $BIN_DIR/bin/samples
-cp -r $AXIS2C_HOME/services/sec_echo $BIN_DIR/bin/samples/server/
-cp -r $AXIS2C_HOME/services/saml_sts $BIN_DIR/bin/samples/server/
-cp -r $AXIS2C_HOME/services/secconv_echo $BIN_DIR/bin/samples/server/
-cp samples/server/sec_echo/services.xml $BIN_DIR/bin/samples/server/sec_echo/services.xml
-cp samples/server/sec_echo/server_axis2.xml $BIN_DIR/bin/samples/server/sec_echo/server_axis2.xml
+cp -r $AXIS2C_HOME/samples/bin/rampartc/* $BIN_DIR/samples/bin/rampartc/
+cp -r $AXIS2C_HOME/samples/lib/rampartc/* $BIN_DIR/samples/lib/rampartc/
+cp -r $AXIS2C_HOME/services/sec_echo $BIN_DIR/services/
+cp -r $AXIS2C_HOME/services/saml_sts $BIN_DIR/services/
+cp -r $AXIS2C_HOME/services/secconv_echo $BIN_DIR/services/
 
 echo "Copy headers"
 cp include/*.h $BIN_DIR/include/$INCL_V_DIR
@@ -91,7 +87,7 @@ cp -rf xdocs/api $BIN_DIR/docs
 echo "Copy sample sources"
 tar -xzf samples/rampart-samples-src*.tar.gz
 rm samples/rampart-samples-src*.tar.gz
-cp -r rampart-samples-src*/* $BIN_DIR/samples
+cp -r rampart-samples-src*/* $BIN_DIR/samples/src/rampartc/
 rm -rf rampart-samples-src*
 
 echo "Copy installer script"
