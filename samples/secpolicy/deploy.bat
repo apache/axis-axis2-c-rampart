d81970ece924e533c8a25c13c51d82fc0ea89a29
Selvaratnam Uthaiyashankar <shankar@apache.org>
2009-07-16 10:54:46 +0000

    Modifying policies according to new signature processing

--- samples/secpolicy/deploy.bat
+++ samples/secpolicy/deploy.bat
@@ -8,4 +8,15 @@ echo Copying %scn%\client policy file
 deploy.js %scn%\client-policy.xml %AXIS2C_HOME%\client_repo\policy.xml
 echo Copying %scn%\services.xml
 deploy.js %scn%\services.xml %AXIS2C_HOME%\services\sec_echo\services.xml
+
+if not exist  %scn%\sts.xml goto no_sts_policy
+echo Copying %scn%\sts.xml
+deploy.js %scn%\sts.xml %AXIS2C_HOME%\services\secconv_echo\services.xml
+
+:no_sts_policy
+if not exist %scn%\rahas_module.xml goto no_rahas_policy
+echo Copying %scn%\rahas_module.xml
+deploy.js %scn%\rahas_module.xml %AXIS2C_HOME%\modules\rahas\module.xml
+
+:no_rahas_policy
 @echo on
