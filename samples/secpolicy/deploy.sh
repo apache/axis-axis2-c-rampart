76f562e60c6554dff8d072d11d7aa406aac76720
Selvaratnam Uthaiyashankar <shankar@apache.org>
2009-10-22 05:15:17 +0000

    no need to copy server's axis2.xml

--- samples/secpolicy/deploy.sh
+++ samples/secpolicy/deploy.sh
@@ -9,9 +9,6 @@ INST_DIR=$AXIS2C_HOME
 CLIENT_REPO="$INST_DIR/client_repo/"
 SERVICE_HOME="$INST_DIR/services/sec_echo"
 
-echo "Copying server's axis2.xml to " $AXIS2C_HOME
-cp ../data/server_axis2.xml $AXIS2C_HOME/axis2.xml
-
 #COPYING THE RELEVENT POLICY FILES TO CLIENT AND SERVER
 
 
