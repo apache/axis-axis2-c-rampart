1d59eb516ebc413a8b591f671ff6c166a90dabf1
Selvaratnam Uthaiyashankar <shankar@apache.org>
2009-10-30 12:45:59 +0000

    Fix for RAMPARTC-138
    Removing client_repo creation

--- samples/build.sh
+++ samples/build.sh
@@ -3,9 +3,7 @@
 ./configure --prefix=${AXIS2C_HOME} --enable-static=no --with-axis2=${AXIS2C_HOME}/include/axis2-1.6.0
 make
 make install
-cd client
-sh deploy_client_repo.sh
-cd ../secpolicy
+cd secpolicy
 sh deploy.sh scenario5
 cd ../
 echo "Copying server's axis2.xml to " $AXIS2C_HOME
